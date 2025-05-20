a541935731baee838dc2862ffb8341e041b3cb1c
I538984 <oliver.schirmer@sap.com>
2025-05-20 11:13:15 +0200

    refactor: baseUrl -> apiBaseUrl

--- src/octokit.ts
+++ src/octokit.ts
@@ -10,14 +10,14 @@ interface ThrottleOptions {
 
 const SmartOctokit = Octokit.plugin(throttling, retry);
 
-export function initOctokit(token?: string, baseUrl?: string): Octokit {
+export function initOctokit(token?: string, apiBaseUrl?: string): Octokit {
   if (!token) {
     throw new Error("GitHub token is required but was not provided");
   }
 
   return new SmartOctokit({
     auth: token,
-    baseUrl,
+    baseUrl: apiBaseUrl,
     throttle: {
       onRateLimit: (
         retryAfter: number,
