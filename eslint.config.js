fe0bb9fc7320c7657a3fc4b2423e0471b8a87acd
John Bain <john.bain@gmail.com>
2026-02-04 00:06:44 +0000

    better lint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: [
+    'dist/',
+    'dist/**',
+    'node_modules/',
+    'coverage/',
+  ],
+  rules: {
+    // Optional but common in Actions repos
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+};
